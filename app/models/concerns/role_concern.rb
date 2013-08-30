b5abacbeb0e99cc38787e51d057219e3f00f874e
Anton Dziga <antonjyha@gmail.com>
2013-08-30 17:31:57 +0300

    add inverse_of to models

--- app/models/concerns/role_concern.rb
+++ app/models/concerns/role_concern.rb
@@ -2,7 +2,7 @@ module RoleConcern
   extend ActiveSupport::Concern
 
   included do
-    has_one :user, :as => :role
+    has_one :user, :as => :role, :inverse_of => :role
     accepts_nested_attributes_for :user
   end
 end

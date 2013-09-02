8cc8d12f162ebb3a0cc46daee56426b3fbb07c10
Anton Dziga <antonjyha@gmail.com>
2013-09-02 14:11:10 +0300

    implement registration

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,3 +1,44 @@
 class Users::RegistrationsController < Devise::RegistrationsController
+  before_filter :prevent_invalid_role_type
 
+  def new
+    @user = User.new
+  end
+
+  def create
+    role = build_role_from_params(find_coach_if_student)
+    if role.save
+      sign_in(:user, role.user)
+      redirect_to :root
+    else
+      @user = role.user
+      render 'new'
+    end
+  end
+
+  private
+  def role_params
+    params.permit(:role_type, :program_code, :user => [:name, :email, :username,
+                  :password, :password_confirmation, :gender, :birthday, :country])
+  end
+
+  def build_role_from_params(extra_params={})
+    role_params[:role_type].classify.constantize.new({:user_attributes => role_params[:user]}.merge(extra_params))
+  end
+
+  def find_coach_if_student
+    if role_params[:role_type].classify == "Player"
+      {:coach => Coach.find_by_program_code(role_params[:program_code])}
+    else
+      {}
+    end
+  end
+
+  def prevent_invalid_role_type
+    unless User::ROLE_TYPES.include?(role_params[:role_type].try(:classify))
+      @user = User.new(role_params[:user])
+      flash[:alert] = 'Invalid account type'
+      render 'new'
+    end
+  end
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -5,4 +5,13 @@ class Player < ActiveRecord::Base
 
   belongs_to :coach
   belongs_to :parent
+
+  after_validation :add_program_code_error_to_user
+
+  private
+  def add_program_code_error_to_user
+    if self.errors[:coach].present? && self.user.present?
+      self.user.errors.add(:program_code, "Invalid program code")
+    end
+  end
 end

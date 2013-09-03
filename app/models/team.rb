87c974d5cf6294129fdd58d6be7b694dd3dad15a
Anton Dziga <antonjyha@gmail.com>
2013-09-03 11:51:32 +0300

    Add seeds for coach, team, players

--- app/models/team.rb
+++ app/models/team.rb
@@ -1,4 +1,7 @@
 class Team < ActiveRecord::Base
+  validates :name, :presence => true, :uniqueness => true
+  validates :coach, :presence => true
+
   has_many :players
   belongs_to :coach
 end
--- spec/models/team_spec.rb
+++ spec/models/team_spec.rb
@@ -3,4 +3,7 @@ require 'spec_helper'
 describe Team do
   it { should have_many(:players) }
   it { should belong_to(:coach) }
+  it { should validate_presence_of(:name) }
+  it { should validate_presence_of(:coach) }
+  it { should validate_uniqueness_of(:name) }
 end

1eed607da221ccea78ff5312a5a48639269875ad
Anton Dziga <antonjyha@gmail.com>
2013-09-13 15:12:36 +0300

    prevent dashboard#show page crushing than no teams

--- app/controllers/coaches/dashboards_controller.rb
+++ app/controllers/coaches/dashboards_controller.rb
@@ -2,6 +2,6 @@ class Coaches::DashboardsController < ApplicationController
   authorize_resource :class => false
   def show
     @team = current_user.role.teams.first
-    @players = @team.players.paginate(:page => params[:page])
+    @players = @team.players.paginate(:page => params[:page]) if @team.present?
   end
 end

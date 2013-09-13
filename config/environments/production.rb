5403262aa19df7425f795b8ab612fd68500a25ca
Anton Dziga <antonjyha@gmail.com>
2013-09-13 14:20:57 +0300

    add maildrillapp for production mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,18 @@ SportsApp::Application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = {:host => 'antonjyha-sportsapp.herokuapp.com', :protocol => 'http'}
+
+  # send emails via gmail account
+  config.action_mailer.smtp_settings = {
+      :address => 'smtp.mandrillapp.com',
+      :port => 587,
+      :enable_starttls_auto => true,
+      :authentication => 'login',
+      :user_name => 'antonjyha@gmail.com',
+      :password => 'erYNoDX89GuJjoRU-xIgaw'
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).

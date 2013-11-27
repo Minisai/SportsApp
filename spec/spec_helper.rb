e925880ee2d919c1d86ce30b4a1673c72efe6ed5
Anton Dziga <antonjyha@gmail.com>
2013-11-27 15:28:49 +0300

    modify database cleaner logic to clean db before running test suite

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -48,6 +48,7 @@ RSpec.configure do |config|
 
   config.before(:suite) do
     DatabaseCleaner.strategy = :deletion
+    DatabaseCleaner.clean_with(:truncation)
   end
   config.before(:each) do
     DatabaseCleaner.start
